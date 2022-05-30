2eee029a57ece34266a936fd067c3438cc9bdb45
Usama <88836532+UsamaMajid95@users.noreply.github.com>
2022-05-30 15:15:30 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,35 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+import requests
+def index(request):
+    appid='7f7eff6835862866057a833262a59194'
+    url="https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    
+    if (request.method=='POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm    
+
+    cities=City.objects.all()
+    
+    all_cities=[]
+
+    for city in cities:
+
+        res = requests.get(url.format(city.name)).json()
+
+        city_info={
+            'city':city.name,
+            'temp':res['main']['temp'],
+            'icon':res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+
+    context={
+        'all_info': all_cities,
+        'form':form
+    }
+    return render(request,'index.html',context)
